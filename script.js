a25fd62ef57a3c3016c2778b628cec927082872f
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2020-12-25 21:54:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//'use strict';
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+//const interface = 'Audio';
+
+//FUNCTIONS
+
+function logger(){
+    console.log('My name is Shreya');
+}
+
+logger();
